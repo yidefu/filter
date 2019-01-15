8c2dadcd2c1d348108ca496eed7f638f8033e036
yindefu <defu.yin@mihoyo.com>
2019-01-15 15:21:50 +0800

    匹配规则

--- example.go
+++ example.go
@@ -10,7 +10,9 @@ import (
 func main() {
 	s := sensitive.SetTreeByFile("sense.txt")
 	now := time.Now()
-	c := s.Replace("测试xxxx", "***", 0)
+	//matchType: 1代表最小匹配规则 其它值为最大匹配
+	//wordNum: 0代表不限制返回数量  其它为返回数量上限
+	c := s.GetBadWord("测试***", 1, 0)
 	fmt.Print(time.Since(now))
 	fmt.Print(c)
 }
